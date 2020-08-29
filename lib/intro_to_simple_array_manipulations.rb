1286bd1dc764877c945b9e3b29bfb954c4b93cd9
Mike Smith <gooseisdead@mac.com>
2020-08-29 21:48:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert(list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"], another_language = ["Python"])
+def using_insert(list_of_programming_languages, another_language)
   another_language.insert(list_of_programming_languages)
 end
 
