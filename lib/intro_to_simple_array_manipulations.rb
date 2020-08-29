8289590646ba15f08532b15debba67b49c00f90a
Mike Smith <gooseisdead@mac.com>
2020-08-29 21:36:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,10 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
+def using_insert
+  list_of_programming_languages.insert(another_language)
+end
+
 
 #my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 #more_favs = ["sports cars", "flatiron school"]
